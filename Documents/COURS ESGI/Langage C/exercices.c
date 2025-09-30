cba916b8709e1d5f8c34f200172f4e86d35bd814
Sarah <sarah.ido777@gmail.com>
2025-09-30 11:09:46 +0200

    Mise à jour du fichier

---
+++ Documents/COURS ESGI/Langage C/exercices.c
@@ -0,0 +1,25 @@
+int main(){
+    // Déterminer un nombre entier
+    int ent1=7; 
+    // Déterminer un nombre à virgule 
+float vir1= .05;
+int ent2= 42;
+// Déterminer un charactère 
+char chr1= '@'; 
+vir1= 13.37;
+ent1=ent2;
+
+exit(EXIT_SUCCES);
+}
+
+:/*Exercice 8*/
+int main(){
+char car; 
+int ent= 7;
+car=4; 
+ent=2;
+printf(car);
+printf(ent);
+printf(car, ent);
+exit(EXIT_SUCCESS);
+}
--- Documents/COURS ESGI/README.md
+++ Documents/COURS ESGI/README.md
@@ -1,1 +1,2 @@
 zzzzzz
+sarah
